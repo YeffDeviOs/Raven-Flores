d3a605ec126b5cd1fb33b3d1de5566cf6a4534ac
Yeff flores <susitoyeff@gmail.com>
2021-05-27 23:28:45 -0300

    my primer app ecommerce

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Tu primera Opción en PCmaster👾👾
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/YeffDeviOs/Raven-Flores"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Make a wish⚛
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
